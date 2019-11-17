301a06841de47c3860dde91fb4dda75b30c6d19a
markmisener <markmisener@gmail.com>
2019-11-17 14:39:17 -0800

    lint

--- cse310-data-structures-and-algorithms/10-elementary-data-structures/10-1-1.py
+++ cse310-data-structures-and-algorithms/10-elementary-data-structures/10-1-1.py
@@ -1,27 +1,27 @@
-"""
-Using Figure 10.1 as a model, illustrate the result of each operation in the
-sequence PUSH(S,4), PUSH(S,1), PUSH(S,3), POP(S), PUSH(S,8), and POP(S) on
-an initially empty stack S stored in array S[1...6].
-"""
+"""Exercise 10."""
+# Using Figure 10.1 as a model, illustrate the result of each operation in the
+# sequence PUSH(S,4), PUSH(S,1), PUSH(S,3), POP(S), PUSH(S,8), and POP(S) on
+# an initially empty stack S stored in array S[1...6].
 from bin import data_structures
 
-S = data_structures.Stack()
-print(S.items)
+if __name__ == "__main__":
+    S = data_structures.Stack()
+    print(S.items)
 
-S.push(4)
-print(S.items)
+    S.push(4)
+    print(S.items)
 
-S.push(1)
-print(S.items)
+    S.push(1)
+    print(S.items)
 
-S.push(3)
-print(S.items)
+    S.push(3)
+    print(S.items)
 
-S.pop()
-print(S.items)
+    S.pop()
+    print(S.items)
 
-S.push(8)
-print(S.items)
+    S.push(8)
+    print(S.items)
 
-S.pop()
-print(S.items)
+    S.pop()
+    print(S.items)
